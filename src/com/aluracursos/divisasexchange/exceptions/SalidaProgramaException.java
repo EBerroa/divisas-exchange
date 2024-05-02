9e46a276cd0104a5689ff3e27bebec5442b84a97
Eliézer Berroa <138054878+EBerroa@users.noreply.github.com>
2024-05-02 16:35:29 -0500

    Add Final Proyect

---
+++ src/com/aluracursos/divisasexchange/exceptions/SalidaProgramaException.java
@@ -0,0 +1,7 @@
+package com.aluracursos.divisasexchange.exceptions;
+
+public class SalidaProgramaException extends Exception{
+    public SalidaProgramaException(String message) {
+        super(message);
+    }
+}
