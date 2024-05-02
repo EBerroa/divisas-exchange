dd06022bf6d723c1518515311115c7998978a9ee
Eliézer Berroa <138054878+EBerroa@users.noreply.github.com>
2024-05-02 16:25:27 -0500

    Delete divisas-exchange/src/com/aluracursos/divisasexchange directory

--- divisas-exchange/src/com/aluracursos/divisasexchange/exceptions/OpcionInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package com.aluracursos.divisasexchange.exceptions;
-
-public class OpcionInvalidaException extends Exception{
-    public OpcionInvalidaException(String message) {
-        super(message);
-    }
-}
--- divisas-exchange/src/com/aluracursos/divisasexchange/exceptions/SalidaProgramaException.java
+++
@@ -1,7 +0,0 @@
-package com.aluracursos.divisasexchange.exceptions;
-
-public class SalidaProgramaException extends Exception{
-    public SalidaProgramaException(String message) {
-        super(message);
-    }
-}
